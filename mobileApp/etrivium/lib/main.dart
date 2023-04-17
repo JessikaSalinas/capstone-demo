bacf5d547bc6e5a77986859255d4752e875f8cc4
Jessika Jimenez <jessikune@gmail.com>
2023-04-17 00:24:51 -0600

    Added bluetooth connection for our prototype app for the prototype demo

--- mobileApp/etrivium/lib/main.dart
+++ mobileApp/etrivium/lib/main.dart
@@ -1,5 +1,22 @@
 import 'package:flutter/material.dart';
+//import 'package:flutter_bluetooth_serial/flutter_bluetooth_serial.dart';
 
+import './MainPage.dart';
+
+void main() => runApp(new ExampleApplication());
+
+class ExampleApplication extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: MainPage());
+  }
+}
+
+
+
+
+// Uncomment everything below and comment out everything above to do other example flutter demo
+/*
 void main() {
   runApp(const MyApp());
 }
@@ -113,3 +130,4 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
+ */
